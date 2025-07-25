02e0912776fbfa3311831e8e7cff7e5ec70b0410
Michał Mościcki <michal.moscicki@faktura.pl>
2025-07-25 10:39:50 +0200

    user service logging and queue

--- user-service/src/main/java/com/moscicki/user_service/cron/OrderNotificationCron.java
+++ user-service/src/main/java/com/moscicki/user_service/cron/OrderNotificationCron.java
@@ -1,8 +1,8 @@
 package com.moscicki.user_service.cron;
 
 import com.moscicki.user_service.manager.SpotNotificationManager;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,7 +13,7 @@ public class OrderNotificationCron {
     @Autowired
     private SpotNotificationManager spotNotificationManager;
 
-    private static final Logger logger = LogManager.getLogger(OrderNotificationCron.class);
+    private static final Logger logger = LoggerFactory.getLogger(OrderNotificationCron.class);
 
     @Scheduled(cron = "0 * * * * *")
     public void orderNotifications() {
--- user-service/src/main/java/com/moscicki/user_service/manager/SpotNotificationManager.java
+++ user-service/src/main/java/com/moscicki/user_service/manager/SpotNotificationManager.java
@@ -1,13 +1,13 @@
 package com.moscicki.user_service.manager;
 
+import com.google.gson.Gson;
 import com.moscicki.user_service.config.RabbitMQConfig;
 import com.moscicki.user_service.dto.notification.OrderNotificationDTO;
-import com.moscicki.user_service.dto.spot.SpotType;
 import com.moscicki.user_service.entities.notification.SpotNotificationDefinition;
-import com.moscicki.user_service.entities.spot.Spot;
 import com.moscicki.user_service.repository.SpotNotificationDefinitionRepository;
 import com.moscicki.user_service.repository.spot.SpotRepository;
-import com.moscicki.user_service.translator.SpotTranslator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +19,8 @@ import java.util.Optional;
 @Service
 public class SpotNotificationManager {
 
+    private static final Logger logger = LoggerFactory.getLogger(SpotNotificationManager.class);
+
     @Autowired
     SpotNotificationDefinitionRepository spotNotificationDefinitionRepository;
     @Autowired
@@ -26,27 +28,33 @@ public class SpotNotificationManager {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
+    private Gson gson = new Gson();
+
     public void orderNotifications() {
         int hour = LocalDateTime.now().getHour();
         int dayOfWeek = LocalDateTime.now().getDayOfWeek().getValue();
 
+
         List<SpotNotificationDefinition> spotNotificationDefinitions = spotNotificationDefinitionRepository.findAllByExecutionHourAndDayOfWeek(hour, dayOfWeek);
+        logger.info("spotNotificationDefinitions.size(): {}", spotNotificationDefinitions.size());
         for (SpotNotificationDefinition spotNotificationDefinition : spotNotificationDefinitions) {
             Optional<OrderNotificationDTO> orderNotificationDTOOptional = createPayload(spotNotificationDefinition);
-            System.out.println("sending to queue");
+            logger.info("Try to send to queue");
             orderNotificationDTOOptional.ifPresent(this::sendToQueue);
             orderNotificationDTOOptional.ifPresent( s -> {
-                System.out.println("senfing-to-queue");
+                System.out.println("sending-to-queue");
             });
             //todo add logging where optional is empty
+            //todo add last generationDate
         }
     }
 
     private void sendToQueue(OrderNotificationDTO dto) {
+        logger.info("Sending to queue. {}", dto.type());
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.EXCHANGE_NAME,
                 RabbitMQConfig.ROUTING_KEY,
-                dto
+                gson.toJson(dto)
         );
     }
 
